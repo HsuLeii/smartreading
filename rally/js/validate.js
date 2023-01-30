00ef3a1cf5d66da0a92be1943a5a384e72f0262c
HsuLeii <ok841113@gmail.com>
2023-01-30 16:14:42 +0800

    0130

--- rally/js/validate.js
+++ rally/js/validate.js
@@ -35,6 +35,14 @@
       materialTitle:{
         presence: true
       },
+
+      processTitle:{
+        presence: true
+      },
+
+      textareaHighlight:{
+        presence: true
+      },
      
     };
 
@@ -67,7 +75,7 @@
     // Updates the inputs with the validation errors
     function showErrors(form, errors) {
       // We loop through all the inputs and show the errors for that input
-      _.each(form.querySelectorAll("input[name], select[name]"), function(input) {
+      _.each(form.querySelectorAll("input[name], select[name],textarea[name]"), function(input) {
         // Since the errors can be null if no errors were found we need to handle
         // that
         showErrorsForInput(input, errors && errors[input.name]);
